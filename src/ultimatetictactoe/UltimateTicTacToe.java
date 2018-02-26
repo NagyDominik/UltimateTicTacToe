7cf297f69afd98b171fec27682e90bfd957379f8
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2018-02-26 13:10:26 +0100

    set window opening order

--- src/ultimatetictactoe/UltimateTicTacToe.java
+++ src/ultimatetictactoe/UltimateTicTacToe.java
@@ -19,7 +19,7 @@ public class UltimateTicTacToe extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("gui/view/MainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gui/view/GameModes.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Ultimate TicTacToe");
